6361d195f0d8d1100658cd8c4e95d1f856ad7c41
joshnfern97 <jfernandez75@gatech.edu>
2022-04-13 09:28:03 -0400

    Updated PCA script and added 15 data

--- PCA.m
+++ PCA.m
@@ -1,18 +1,24 @@
-%% 0 Degree Lifts
 
-%subject_array = {'04','05','06','07','09','10','15'};
-subject_array = {'04'};
-% trial_array = {'01','02','03','04','05','06','07'};
-trial_array = {'01'};
+
+subject_array = {'04','05','06','07','09','10','15'};
+%subject_array = {'04'};
+trial_array = {'01','02','03','04','05','06','07'};
+%trial_array = {'01'};
 
 total_data = [];
 
+degree = 180;
 
 for i = 1:length(subject_array)
     for j = 1:length(trial_array)
+        if degree ==0
         file = ['consolidated_channel_data\',subject_array{i},'\DOE_Biomech_',subject_array{i},'_L_0_KW_',trial_array{j},'.csv']
+        else
+        file = ['consolidated_channel_data\',subject_array{i},'\DOE_Biomech_',subject_array{i},'_L_',num2str(degree),'_R_KW_',trial_array{j},'.csv']   
+            
+        end
         data = readtable(file);
-
+        
 
         total_data = [total_data;data];
     end
@@ -30,7 +36,7 @@ for i = 1:length(headers)
    name = headers{i};
    plot(channels.(name))
    message = [name, ':',num2str(i)];
-   title(messagePCA)
+   title(message)
     
     
 end
@@ -42,29 +48,42 @@ joint_torques = total_data(:,end-2:end);
 % Find eigenvalues and vectors
 
 array = table2array(channels);
-x = zscore(array)
+x = zscore(array);
 cov_data = cov(x);
 
 [V,D] = eig(cov_data);
 [eig_val,I] = sort(diag(D),'descend');
 loading = V(:,I);
 
-percent_var = eig_val(1:3)./sum(eig_val);
+PCA_num = 3;
+percent_var = eig_val(1:PCA_num)./sum(eig_val);
+total_var_explained = sum(percent_var)
 figure(2)
-bar(percent_var)
+b = bar(percent_var);
+b.FaceColor = 'flat';
+colors = [1 0 0;
+          0 0 1;
+          .5 0 .5];
+b.CData(1,:) = colors(1,:);
+b.CData(2,:) = colors(2,:);
+b.CData(3,:) = colors(3,:);
+xlabel('PCA')
+ylabel('Percent Variance')
 title('Percent Variance of First 3 PCs')
 
 
-%Plot Loading of PC1
-figure(3)
-bar(V(:,1))
-xlabel('Data Features')
-title('Loading for PC1')
-figure(4)
-bar(V(:,2))
-xlabel('Data Features')
-title('Loading for PC2')
 
+for i = 1:PCA_num
+    figure(i+2)
+    b = bar(V(:,i));
+    b.FaceColor = 'flat';
+    b.CData = colors(i,:);
+    xlabel('Data Features')
+    message = ['Loading for PC',num2str(i)];
+    title(message)
+
+
+end
 
 
 
